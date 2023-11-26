065cdf905303b6ecae96857e366d0be185594a15
Cédric Honorat <chonorat@student.42lyon.fr>
2023-11-26 18:22:44 +0100

    Map flickering fixed

--- Sources/Move/move_player.c
+++ Sources/Move/move_player.c
@@ -6,7 +6,7 @@
 /*   By: chonorat <chonorat@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 22:48:06 by chonorat          #+#    #+#             */
-/*   Updated: 2023/11/26 00:45:07 by chonorat         ###   ########.fr       */
+/*   Updated: 2023/11/26 18:15:52 by chonorat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,21 @@ static void	move_lr(t_data *data)
 {
 	if (data->player.move.l_move)
 	{
-		if (data->player.move.sprint && !wall_hit(data, data->player.x_pos - (0.003 * (data->screen_res[1] >> 7)),
+		if (data->player.move.sprint && !wall_hit(data, data->player.x_pos - (0.005 * (data->screen_res[1] >> 7)),
 			data->player.y_pos))
-			data->player.x_pos -= 0.003 * (data->screen_res[1] >> 7);
-		else if (!data->player.move.sprint && !wall_hit(data, data->player.x_pos - (0.0015 * (data->screen_res[1] >> 7)),
+			data->player.x_pos -= 0.005 * (data->screen_res[1] >> 7);
+		else if (!data->player.move.sprint && !wall_hit(data, data->player.x_pos - (0.0025 * (data->screen_res[1] >> 7)),
 			data->player.y_pos))
-			data->player.x_pos -= 0.0015 * (data->screen_res[1] >> 7);
+			data->player.x_pos -= 0.0025 * (data->screen_res[1] >> 7);
 	}
 	if (data->player.move.r_move)
 	{
-		if (data->player.move.sprint && !wall_hit(data, data->player.x_pos + (0.003 * (data->screen_res[1] >> 7)),
+		if (data->player.move.sprint && !wall_hit(data, data->player.x_pos + (0.005 * (data->screen_res[1] >> 7)),
 			data->player.y_pos))
-			data->player.x_pos += 0.003 * (data->screen_res[1] >> 7);
-		else if (!data->player.move.sprint && !wall_hit(data, data->player.x_pos + (0.0015 * (data->screen_res[1] >> 7)),
+			data->player.x_pos += 0.005 * (data->screen_res[1] >> 7);
+		else if (!data->player.move.sprint && !wall_hit(data, data->player.x_pos + (0.0025 * (data->screen_res[1] >> 7)),
 			data->player.y_pos))
-			data->player.x_pos += 0.0015 * (data->screen_res[1] >> 7);
+			data->player.x_pos += 0.0025 * (data->screen_res[1] >> 7);
 	}
 }
 
@@ -47,19 +47,19 @@ void	move_player(t_data *data)
 	if (data->player.move.f_move)
 	{
 		if (data->player.move.sprint && !wall_hit(data, data->player.x_pos,
-			data->player.y_pos - (0.003 * (data->screen_res[1] >> 7))))
-			data->player.y_pos -= 0.003 * (data->screen_res[1] >> 7);
+			data->player.y_pos - (0.005 * (data->screen_res[1] >> 7))))
+			data->player.y_pos -= 0.005 * (data->screen_res[1] >> 7);
 		else if (!data->player.move.sprint && !wall_hit(data, data->player.x_pos,
-			data->player.y_pos - (0.0015 * (data->screen_res[1] >> 7))))
-			data->player.y_pos -= 0.0015 * (data->screen_res[1] >> 7);
+			data->player.y_pos - (0.0025 * (data->screen_res[1] >> 7))))
+			data->player.y_pos -= 0.0025 * (data->screen_res[1] >> 7);
 	}
 	if (data->player.move.b_move)
 	{
 		if (data->player.move.sprint && !wall_hit(data, data->player.x_pos,
-			data->player.y_pos + (0.003 * (data->screen_res[1] >> 7))))
-			data->player.y_pos += 0.003 * (data->screen_res[1] >> 7);
+			data->player.y_pos + (0.005 * (data->screen_res[1] >> 7))))
+			data->player.y_pos += 0.005 * (data->screen_res[1] >> 7);
 		else if (!data->player.move.sprint && !wall_hit(data, data->player.x_pos,
-			data->player.y_pos + (0.0015 * (data->screen_res[1] >> 7))))
-			data->player.y_pos += 0.0015 * (data->screen_res[1] >> 7);
+			data->player.y_pos + (0.0025 * (data->screen_res[1] >> 7))))
+			data->player.y_pos += 0.0025 * (data->screen_res[1] >> 7);
 	}
 }
