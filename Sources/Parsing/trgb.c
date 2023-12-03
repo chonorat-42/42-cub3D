4391ada52c9ae72566f2856efdd0c4842a6c5772
pgouasmi <pgouasmi@gmail.com>
2023-12-03 18:48:38 +0100

    rgb colors added to data

--- Sources/Parsing/trgb.c
+++ Sources/Parsing/trgb.c
@@ -6,20 +6,19 @@
 /*   By: pgouasmi <pgouasmi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:42:00 by pgouasmi          #+#    #+#             */
-/*   Updated: 2023/12/03 18:19:25 by pgouasmi         ###   ########.fr       */
+/*   Updated: 2023/12/03 18:47:25 by pgouasmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-int convert_trgb(int *colors)
+int	convert_trgb(int *colors, int transparency)
 {
-	return (0 << 24 | colors[0] << 16 | colors[1] << 8 | colors[2]);
+	return (transparency << 24 | colors[0] << 16 | colors[1] << 8 | colors[2]);
 }
 
 void	get_trgb(t_data *data, t_parser *parser)
 {
-	(void)data;
-	int color = convert_trgb(parser->colors.c_colors);
-	printf("colors = %d\n", color);
+	data->colors.ceiling = convert_trgb(parser->colors.c_colors, 0);
+	data->colors.floor = convert_trgb(parser->colors.f_colors, 0);
 }
