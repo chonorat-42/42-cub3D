7c3e3f8483e1ed819060cb934bb10410bda43d91
Pascal Gouasmi <pgouasmi@z1r6p4.42lyon.fr>
2023-12-07 12:34:08 +0100

    pathfinding fixed to not have collision issues for diagonal

--- Sources/pathfinding.c
+++ Sources/pathfinding.c
@@ -6,7 +6,7 @@
 /*   By: pgouasmi <pgouasmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 15:53:34 by pgouasmi          #+#    #+#             */
-/*   Updated: 2023/12/07 12:22:16 by pgouasmi         ###   ########.fr       */
+/*   Updated: 2023/12/07 12:33:01 by pgouasmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -180,11 +180,11 @@ int best_path(t_data *data, int y, int x, t_ennemy *ennemy)
 	if (ennemy->pos[0] && !ft_ischarinset(data->map[ennemy->pos[0] - 1][ennemy->pos[1]], "1?!"))
 	{
 		arr[0][NORTH] = (long long)pow(difference(ennemy->pos[0] - 1, y), 2) + (long long)pow(difference(ennemy->pos[1], x), 2);
-		if (ennemy->pos[0] && data->map[0][ennemy->pos[1] + 1] && !ft_ischarinset(data->map[ennemy->pos[0] - 1][ennemy->pos[1] + 1], "1?!"))
+		if (ennemy->pos[0] && data->map[0][ennemy->pos[1] + 1] && !ft_ischarinset(data->map[ennemy->pos[0] - 1][ennemy->pos[1] + 1], "1?!") && !ft_ischarinset(data->map[ennemy->pos[0]][ennemy->pos[1] + 1], "1?!"))
 			arr[0][NE] = (long long)pow(difference(ennemy->pos[0] - 1, y), 2) + (long long)pow(difference(ennemy->pos[1] + 1, x), 2);
 		else
 			arr[0][NE] = 18446744073709551;
-		if (ennemy->pos[0] && ennemy->pos[1] - 1 && !ft_ischarinset(data->map[ennemy->pos[0] - 1][ennemy->pos[1] - 1], "1?!"))
+		if (ennemy->pos[0] && ennemy->pos[1] - 1 && !ft_ischarinset(data->map[ennemy->pos[0] - 1][ennemy->pos[1] - 1], "1?!") && !ft_ischarinset(data->map[ennemy->pos[0]][ennemy->pos[1] - 1], "1?!"))
 			arr[0][NW] = (long long)pow(difference(ennemy->pos[0] - 1, y), 2) + (long long)pow(difference(ennemy->pos[1] - 1, x), 2);
 		else
 			arr[0][NW] = 18446744073709551;
@@ -200,7 +200,7 @@ int best_path(t_data *data, int y, int x, t_ennemy *ennemy)
 	if (data->map[ennemy->pos[0] + 1] && !ft_ischarinset(data->map[ennemy->pos[0] + 1][ennemy->pos[1]], "1?!"))
 	{
 		arr[0][SOUTH] = (long long)pow(difference(ennemy->pos[0] + 1, y), 2) + (long long)pow(difference(ennemy->pos[1], x), 2);
-		if (data->map[ennemy->pos[0] + 1] && data->map[0][ennemy->pos[1] + 1] && !ft_ischarinset(data->map[ennemy->pos[0] + 1][ennemy->pos[1] + 1], "1?!"))
+		if (data->map[ennemy->pos[0] + 1] && data->map[0][ennemy->pos[1] + 1] && !ft_ischarinset(data->map[ennemy->pos[0] + 1][ennemy->pos[1] + 1], "1?!") && !ft_ischarinset(data->map[ennemy->pos[0]][ennemy->pos[1] - 1], "1?!"))
 			arr[0][SE] = (long long)pow(difference(ennemy->pos[0] + 1, y), 2) + (long long)pow(difference(ennemy->pos[1] + 1, x), 2);
 		else
 			arr[0][SE] = 18446744073709551;
@@ -279,8 +279,8 @@ void	init_ennemy(t_data *data, t_ennemy *ennemy, char **map)
 		j++;
 	}
 	fill_mask(ennemy->mask, map);
-	ennemy->pos[0] = 1;
-	ennemy->pos[1] = 10;
+	ennemy->pos[0] = 3;
+	ennemy->pos[1] = 2;
 	ennemy->d_pos[0] = ennemy->pos[0] + 0.5;
 	ennemy->d_pos[1] = ennemy->pos[1] + 0.5;
 	// printf("ennemy x = %d, y = %d\n", data->ennemy.pos[1], data->ennemy.pos[0]);
